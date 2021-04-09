48a871387a20832b2cc2951d082e831bc1dfecdb
DESKTOP-5L7N2A8\ib <lemaire.romain37@gmail.com>
2021-04-09 15:03:26 +0200

    salut

---
+++ Hello_World_Bitch/src/fr/ibformation/helloworld/Laucher.java
@@ -0,0 +1,16 @@
+package fr.ibformation.helloworld;
+
+import java.util.Scanner;
+
+public class Laucher {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World Bitches and Renaud !");
+
+	
+		Scanner scanner;
+		
+		
+	}
+
+}
